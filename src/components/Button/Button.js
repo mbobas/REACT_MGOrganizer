71466e32426643ccb298a22d936560d9bac50d3d
Michal <mbobas@gmail.com>
2021-04-29 18:59:30 +0200

    dry function in styled component added

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,26 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const Button = styled.button`
-background-color: ${({secondary})=> secondary ? '#E6E6E6':  '#FFD82B' };
-width: ${({secondary})=> secondary ? '105px':  '220px' };
-height: ${({secondary})=> secondary ? '30px':  '47px' };
+background-color: #E6E6E6;
+width: 220px;
+height: 47px;
 border: none;
 border-radius: 50px;
 font-family: "Montserrat";
 font-weight: 500;
-font-size: ${({secondary})=> secondary ? '10px':  '16px' };
+font-size: 16px;
 text-transform: uppercase;
+
+
+${({secondary})=> (
+    secondary && css`
+    background-color: #FFD82B;
+    width: 105px;
+    height: 30px;
+    font-size: 10px;
+    `
+)
+}
 `;
 
 export default Button;
