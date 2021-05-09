e08e07d45f5a578b09f4217636b29d3096311bf0
Michal <mbobas@gmail.com>
2021-05-09 11:20:04 +0200

    confiurguration withkobs but something dosent working_2

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,27 +1,16 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/button-has-type */
+/* eslint-disable spaced-comment */
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 // Button.stories.js
 
 // // MyComponent.stories.js | MyComponent.stories.ts
-import { withKnobs, select, text, boolean} from '@storybook/addon-knobs';
+import { withSmartKnobs, withKnobs, select, text, boolean} from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
 import Button from './Button';
-
-// export const Knobs = () => (
-// <Button disabled={boolean('Disabled', false)}>{text('label', 'button labell')}</Button>
-// );
-
-const label = 'Colors';
-const options = {
-  Primary: 'hsl(49,100%,58%)',
-  Secondary: 'hsl(196,83%,75%)',
-  Tertiary: 'hsl(106,47%,64%)',
- 
-};
-const defaultValue = 'hsl(49,100%,58%)';
-const groupId = 'GROUP-ID1';
-
-const value = select(label, options, defaultValue, groupId);
 
 export default {
   title: 'Components/Button',
@@ -43,5 +32,17 @@ export const Third = Template.bind({
 Third.args = { width: "500px" }
 
 export const Four = Template.bind({});
-Four.addDecorator(withKnobs)
-Four.args = {color: options}
+const label = 'Color';
+const color = {
+  Primary: 'hsl(49,100%,58%)',
+  Secondary: 'hsl(196,83%,75%)',
+  Tertiary: 'hsl(106,47%,64%)',
+ 
+};
+const defaultValue = 'hsl(49,100%,58%)';
+const groupId = 'GROUP-ID1';
+
+const value = select(label, color, defaultValue, groupId);
+Four.args = {
+  
+};
